fb025356d32b55f0f5ea2f3a0b4770330d41db66
Nafis <nafis88@gmail.com>
2017-08-31 01:46:11 -0700

    no message

--- array_sum.c
+++ array_sum.c
@@ -27,7 +27,7 @@ int main(void)
 {
    int arr[] = {2, 3, 4, 10, 40};
    int n = sizeof(arr)/ sizeof(arr[0]);
-   int x = 40;
+   int x = 4;
    int result = binarySearch(arr, 0, n-1, x);
    (result == -1)? printf("Element is not present in array")
                  : printf("Element is present at index %d", result);
