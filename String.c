f72773488023e1d1614dd6347192d13088c5da03
Nafis <nafis88@gmail.com>
2017-10-12 03:13:05 -0700

    Books and String

--- String.c
+++ String.c
@@ -9,6 +9,9 @@ int main() {
   s2 = "efgh";
   s3 = "abcd";
 
+  char s6[] = "asdf";
+  char s7 [10];
+
   printf("%s %s %s\n",s1, s2, s3 );
 
   printf("%d\n",strcmp (s1, s3) ); //output is 0 as s1 and s3 are identical
@@ -21,6 +24,13 @@ int main() {
   address = strstr (s4, s5);
   printf("%s %s\n", address, strstr(s4, s5)  );
 
+  printf("%s %s \n",s7, strcpy (s7, s6) ); //have to use array type declaration
+
+  int i =0;
+  for ( i = 0; s4[i] != '\0'; i++) {
+    /* code */
+    printf("%c %d\n",s4[i], i );
+  }
 
   return 0;
 }
