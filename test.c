4a2c40fba0dfac1e0218aaf3d2f63c6a625cacb9
Md Nafis Khan <nafis88@gmail.com>
2017-11-09 01:53:57 -0800

    no message

--- test.c
+++ test.c
@@ -6,22 +6,44 @@
 int main()
 {
 
-  char binary[] = "110000";
+  char binary[] = "0110001";
   char newBinary[1000];
   //printf("%s\n", binary); 
 
   int length = strlen(binary);
 
-  int i;
+  int i = 0;
   char temp = '0';
+  //printf("%c\n",binary[length-1] );
 
-  strcpy (newBinary, binary);
+
+  for (i = 0 ; i < length ; i++)
+  {
+    if (binary[i] == '0' && i == 0)
+      continue;
+    else
+    newBinary[i] = binary[i]; 
+    printf("%c %d\n",newBinary[i] , i); 
+    newBinary[length] = '\0';     
+
+  }
+  
+  printf("%d\n",strlen(binary) );
+  //strcpy (newBinary, binary);
+  i = 0;
+  while (newBinary[i] !='\0')
+  {
+    printf("%c\n", newBinary[i]);
+    i++;
+  }
+
   printf(" Original %s\n",newBinary );
 
   for (i = length-1; i >=0 ; i--)
   
   {
     temp = binary [i];
+    printf("%c\n", temp);
 
     if (temp == '0')
     {
